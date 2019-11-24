4d7dfef19c9b9d15a1ba7bba79eb5dc531c9bb89
ppambler <allinlbj@gmail.com>
2019-11-24 12:25:10 +0800

    添加中间件，用于校验输入

--- 01-decorator-TodoList/src/index.js
+++ 01-decorator-TodoList/src/index.js
@@ -1,5 +1,6 @@
 import tools from './utils/tools.js';
 import todo from './decorator/todo.js';
+import M from './middleware/functions.js';
 
 @todo
 @tools
@@ -53,24 +54,52 @@ class Todo {
     // console.log(oParent) // li元素
     // console.log(val) //内容
     this.addLog(val, 'remove')
-    oParent.remove()
-    this.oLogList.appendChild(this._showLog(this.iterator.next().value))
+    this._handleItem('remove', {
+      val,
+      iteratorVal: this.iterator.next().value,
+      oParent
+    })
+  }
+  // _handleItem这个函数命名加了_表示这是额外扩展的函数，不过一般都用于私有函数的命名！
+  // 总之，这样的函数在最初实现功能的时候是咩有的，或者是这是为了重构代码而存在的函数！咩有这个函数也能实现相应的功能！
+  _handleItem(method, options) {
+    switch (method) {
+      case 'add':
+        this.oTodoList.appendChild(this._createItem(options.val))
+        this.oLogList.appendChild(this._showLog(options.iteratorVal))
+        this.oInput.value = ''
+        break;
+      case 'remove':
+        options.oParent.remove()
+        this.oLogList.appendChild(this._showLog(options.iteratorVal))
+        break;
+      default:
+        break;
+    }
+
   }
   addItem() {
     const val = Todo.trimSpace(this.oInput.value)
-    // console.log(val)
-    if (val.length <= 0 || val.length > 20) {
-      alert('字数 1~20 个')
-      return
-    }
-    if (this._checkItemExist(val, this.log) === -1) {
-      alert('已存在该项，请不要重复添加！')
-      return
-    }
-    this.addLog(val, 'add')
-    this.oTodoList.appendChild(this._createItem(val))
-    this.oLogList.appendChild(this._showLog(this.iterator.next().value))
-    this.oInput.value = ''
+    M([function (next) {
+      if (val.length <= 0 || val.length > 20) {
+        alert('字数 1~20 个')
+        return false
+      }
+      next()
+    }, (next) => {
+      console.log(this)
+      if (this._checkItemExist(val, this.log) === -1) {
+        alert('已存在该项，请不要重复添加！')
+        return false
+      }
+      next()
+    }, () => {
+      this.addLog(val, 'add')
+      this._handleItem('add', {
+        val,
+        iteratorVal: this.iterator.next().value
+      })
+    }])
   }
 
   addLog(value, action) {
