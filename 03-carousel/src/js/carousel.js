d1e84c643cc543d1bf99e7f81036cb13d3a1aa0b
ppambler <allinlbj@gmail.com>
2019-12-12 23:39:37 +0800

    鼠标移入停止轮播，移出继续轮播

--- 03-carousel/src/js/carousel.js
+++ 03-carousel/src/js/carousel.js
@@ -14,7 +14,10 @@ class Carousel {
     this.bindEvent()
   }
   bindEvent() {
-
+    // .on( events [, selector ] [, data ], handler(eventObject) )
+    // 第二个参数是个子节点，如果不写，那么你在这里传个对象，那么这个数据就会交给事件处理函数的event.data
+    this.$dom.on('mouseover', { event: 'in' }, $.proxy(this.mouseInOut, this))
+    this.$dom.on('mouseout', { event: 'out' }, $.proxy(this.mouseInOut, this))
   }
   autoPlay() {
     // console.log($) //把全局的$变成Carousel实例的$属性是否会比较好点呢？或者我可以用bind哈！
@@ -26,6 +29,20 @@ class Carousel {
     // 自动轮播，显然是下一张图片，而不是上一张
     this._setIndex('next')
     this._pageChange(this.curIdx)
+  }
+  mouseInOut(e) {
+    console.log(e)
+    var event = e.data.event
+    switch (event) {
+      case 'in':
+        clearInterval(this.timer)
+        break;
+      case 'out':
+        this.autoPlay()
+        break;
+      default:
+        break;
+    }
   }
   _setIndex(dir) {
     switch (dir) {
